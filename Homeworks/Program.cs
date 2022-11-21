816784a9871bb87f3b4326a229bdf0c3a5d0860a
darya <feoktistova.darya2017@yandex.ru>
2022-11-21 21:10:06 +0700

    DZ

---
+++ Homeworks/Program.cs
@@ -0,0 +1,80 @@
+﻿ /*задача 1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее*/
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+     Console.WriteLine($"{num1} > {num2}");
+}
+else
+{
+    Console.WriteLine($"{num2} > {num1}");
+}*/
+
+
+/*Задача2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+while (max < num2)
+{
+max=num2;
+}
+    if (max < num3)
+    {
+        max=num3;
+        Console.WriteLine($"{max} -> number Max");
+    }
+    else
+    {
+        Console.WriteLine($"{max} -> number Max");
+    }*/
+
+
+/*Задача3  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка). */
+
+/*Console.WriteLine("Input numberN");
+int numN = Convert.ToInt32(Console.ReadLine());
+if (numN<0)
+    if (numN % 2==(-1))
+    {
+    Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }   
+else
+{
+    if (numN % 2==1)
+    {
+    Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }   
+}*/
+
+
+/*Задача 4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+/*Console.WriteLine("Input numberN");
+int numN = Convert.ToInt32(Console.ReadLine());
+int num2 = 2;
+int count = 0 + num2;
+while (count <= numN)
+{
+    Console.WriteLine(count);
+    count++;
+    count++;
+}*/
+
+
