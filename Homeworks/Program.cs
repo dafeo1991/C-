07f3a5915e0964de6e9de3068496268e556b6bbc
darya <feoktistova.darya2017@yandex.ru>
2022-11-25 14:46:10 +0700

    new commit3

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -32,29 +32,30 @@ Console.WriteLine(max);*/
 
 /*Задача3  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка). */
 
-/*Console.WriteLine("Input numberN");
-int numN = Convert.ToInt32(Console.ReadLine());
-if (numN<0)
-    if (numN % 2==(-1))
-    {
-    Console.WriteLine("число нечетное");
-    }
-    else
-    {
-    Console.WriteLine("Число четное");
-    }   
-else
-{
-    if (numN % 2==1)
-    {
-    Console.WriteLine("число нечетное");
-    }
-    else
-    {
-    Console.WriteLine("Число четное");
-    }   
-}*/
-
+// не оптимальное решение
+// Console.WriteLine("Input numberN");
+// int numN = Convert.ToInt32(Console.ReadLine());
+// if (numN<0)
+//     if (numN % 2==(-1))
+//     {
+//     Console.WriteLine("число нечетное");
+//     }
+//     else
+//     {
+//     Console.WriteLine("Число четное");
+//     }   
+// else
+// {
+//     if (numN % 2==1)
+//     {
+//     Console.WriteLine("число нечетное");
+//     }
+//     else
+//     {
+//     Console.WriteLine("Число четное");
+//     }   
+// }
+// Console.WriteLine(-5 % 2);
 
 /*Задача 4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
 
@@ -69,3 +70,60 @@ while (count <= numN)
 }*/
 
 
+// ДЗ №2 
+/* задание 1
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.*/
+
+/*int ThreeDigitNumber (int num)
+{
+    int sot = num % 100;
+    int sot2 = sot /10;  
+    return sot2;
+
+}
+
+int random = new Random().Next(100, 1000);
+int sot2 = ThreeDigitNumber(random);
+Console.WriteLine($"New version of {random} is {sot2}");*/
+
+/*Задание 2
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+/*Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+string num2 = Convert.ToString(num);
+if ( num2.Length > 2)
+{
+  Console.WriteLine("Figure № 3 -> " + num2[2]);
+}
+else
+{
+  Console.WriteLine("There are no numbers");
+}*/
+
+
+
+
+//Задача 3  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*Console.WriteLine("Enter a number from 1 to 7");
+int num = Convert.ToInt32(Console.ReadLine());
+bool day (int num )
+
+{
+    if (num > 5 && num < 8)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine($"{day(num)}");*/
+
+
+
+
+   
--- seminars/seminar2/Program.cs
+++ seminars/seminar2/Program.cs
@@ -1,96 +1,69 @@
-﻿/*Console.WriteLine("Input numberN");
-int numN = Convert.ToInt32(Console.ReadLine());
-int count = numN * -1;
-while (count <= numN)
-{
-    Console.WriteLine(count);
-    count++; 
-}*/
+﻿//Находим большую цифру из числа, рандомно находим число
 
-/*Console.WriteLine("Input number N");
-int numN = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(numN % 10);*/ 
-
-/*Console.WriteLine("Input number N");
-int numN = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Math.Abs(numN)%10);*/
-
-/*Console.WriteLine("Input number");
-int num = Convert.ToInt32(Console.ReadLine());
-int quad = num * num; 
-Console.Write($"Quad of {num} -> {quad} ");*/
-
-/*Console.WriteLine("Input number 1");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number 2");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int quad = num2 * num2;
-if (quad == num1)
+/*int FindBiggestDigit (int num)
 {
-    Console.WriteLine($"{num1} is quad of {num2}");
-}
-else
-{
-Console.WriteLine($"{num1} is not quad of {num2}");
-}*/
-    
-/*задача 1
-Console.WriteLine("Input number 1");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number 2");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2)
-{
-     Console.WriteLine($"{num1} > {num2}");
-}
-else
-{
-    Console.WriteLine($"{num2} > {num1}");
-}*/
-
-/*Задача2
-Console.WriteLine("Input number 1");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number 2");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number 3");
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-while (max < num2)
-{
-max=num2;
-}
-    if (max < num3)
+    int ed = num % 10;
+    int dec = num / 10;
+    if (ed > dec)
     {
-        max=num3;
-        Console.WriteLine($"{max} -> number Max");
+        return ed;
+    }
+    else 
+    {
+        return dec;
+    }
+}
+
+int random = new Random().Next(10, 100);
+int result = FindBiggestDigit(random);
+Console.WriteLine($"Your number -> {random}");
+Console.WriteLine($"The biggest digit -> {result}");*/
+
+// Вывести случайное число и удалить вторую цифру
+
+/*int CutNumber (int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    int res = sot * 10 + ed;
+    return res;
+}
+
+int random = new Random().Next(100, 1000);
+int NewNumber = CutNumber(random);
+Console.WriteLine($"New version of {random} is {NewNumber}");*/
+
+// Принимает два числа и проверяет кратно ли второе число первому
+
+/*void main (int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine (num1 / num2);
+        Console.WriteLine("Число кратно");
+    }
+
+    else
+    {
+        Console.WriteLine("Число не кратно -> остаток от деления");
+        Console.WriteLine(num1 % num2);
+    }
+}
+main (17, 4);*/
+
+// принимает на вход число и проверяет кратно ли числам 7 и 23
+/*bool main (int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        return true;
     }
     else
     {
-        Console.WriteLine($"{max} -> number Max");
-    }*/
-
-
-
-Console.WriteLine("Input numberN");
-int numN = Convert.ToInt32(Console.ReadLine());
-if (numN<0)
-    if (numN % 2==(-1))
-    {
-    Console.WriteLine("число нечетное");
+        return false;
     }
-    else
-    {
-    Console.WriteLine("Число четное");
-    }   
-else
-{
-    if (numN % 2==1)
-    {
-    Console.WriteLine("число нечетное");
-    }
-    else
-    {
-    Console.WriteLine("Число четное");
-    }   
 }
+Console.WriteLine($"{main(161)}");*/
+
+
+
