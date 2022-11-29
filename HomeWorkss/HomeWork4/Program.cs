51d28ae98692d0b3716dfff198d04749c0b8a80a
darya <feoktistova.darya2017@yandex.ru>
2022-11-29 14:20:24 +0700

    29.11

---
+++ HomeWorkss/HomeWork4/Program.cs
@@ -0,0 +1,62 @@
+﻿//принимает два числа, число А умножить само на себя В раз
+
+// int Num (int A, int B)
+// {   
+//     int i = 1;
+//     int sum = A;
+//     while (i < B)
+//     {
+//          sum =  sum * A;
+//          i++;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Iptum number A");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Iptum number B");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Num(numA, numB));
+
+//принимает на вход число и выдаёт сумму цифр в числе.
+
+// int Num2(int num)
+// {
+//     int result = 0;
+//     while (num > 0 || num < 0)
+//     {
+//         result = result + num % 10;
+//         num = num / 10;
+
+//     }
+//     return result;
+// }
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Num2(num));
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int[] Array(int min, int max)
+// {
+//     int[] newArray = new int[8];
+//     for (int i = 0; i < 8; i++)
+//     {
+//         newArray[i] = new Random().Next(min, max);
+//     }
+//     return newArray;
+// }
+// void New2(int[] array)
+// {
+//     Console.Write("Получившийся массив -> ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// Console.WriteLine("Введите число min");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число max");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int [] myArray = Array(min, max);
+// New2(myArray); 
---
+++ seminars/seminar4/Program.cs
@@ -0,0 +1,89 @@
+﻿// Принимает число А и выдает сумму чисел от 1 до А
+// int Findsum (int a)
+// {
+//     int sum = 0;
+//     {
+//         for (int i = 1; i <= a; i++)
+//         {
+//             sum = sum + i;
+//         }
+//         return sum;
+//     }
+// }
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = Findsum(number);
+// Console.WriteLine(result);
+
+//принимает число и выдает количество цифр в числе (делим на 10 пока остаток не будет < 0  и считаем кол во раз)
+
+// int CountDigit (int num)
+// {
+//     int count = 0;
+//     while (num>0)
+//     {
+//         num = num / 10;
+//         count ++;
+//     }
+//     return count;
+// }
+// Console.WriteLine("Iptum number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(CountDigit(number));
+
+// Принимает число N выдает произведение чисел от 1 до  N (факториал)
+
+// int Fak (int num)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//          result = i * result;
+//     }
+//     return result;
+// }
+// Console.WriteLine("Iptum number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Fak(number));
+
+// выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке
+
+// void FilArray (int size)
+// {
+//     int[] array = new int [size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(0,2);
+//         Console.Write($"{array[i]}");
+//     }
+// }
+// FilArray(8);
+
+int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("Получившийся массив ->");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(a, min, max);
+ShowArray(myArray);
