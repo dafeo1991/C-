a34e5e931e876038f38eb0a13e438aae337ae100
darya <feoktistova.darya2017@yandex.ru>
2022-12-01 01:00:29 +0700

    3

--- HomeWorkss/HomeWork6/Program.cs
+++ HomeWorkss/HomeWork6/Program.cs
@@ -37,25 +37,6 @@
 // Sum(myArray);
 
 //  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// способ1
-
-// double Coordinates(double k1, double b1, double k2, double b2)
-// {
-//     double x = (b2 - b1) / (k1 - k2);
-//     return x;
-//     double y = k1 * (b2 - b1) / (k1 - k2) + b1;
-//     return y;
-// }
-
-// Console.WriteLine("Введите координаты k1, b1, k2, b2");
-// double k1 = Convert.ToDouble(Console.ReadLine());
-// double b1 = Convert.ToDouble(Console.ReadLine());
-// double k2 = Convert.ToDouble(Console.ReadLine());
-// double b2 = Convert.ToDouble(Console.ReadLine());
-// double x = Coordinates(k1, b1, k2, b2);
-// double y = Coordinates(k1, b1, k2, b2);
-// Console.WriteLine($"Точка пересечения двух прямых -> x {x}, y {y}");
-
 //Способ 2
 
 // void Coordinates(double k1, double b1, double k2, double b2)
